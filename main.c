301dce5cd7774fdf257977309270bbc8c68b8d2e
mahmoudkhera <113945592+mahmoudkhera@users.noreply.github.com>
2023-12-05 17:09:53 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,11 @@
 
 int main(int argc,char *argv[])
 {
-    int _13m =0;
     char *store_code=NULL;
 
     clock_t start_time = clock();
 
-    read_code(&store_code,argv[1]); // argv to read the input file fromm cmd
+    read_code(&store_code,argv[1]); //   read the input file fromm cmd
 
     printf("%s",store_code);
    tokens_genrator(store_code);
