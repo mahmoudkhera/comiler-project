c167dcea748bea3ed8e74907802cc465fdc8d389
mahmoudkhera <113945592+mahmoudkhera@users.noreply.github.com>
2023-12-08 13:31:12 +0200

    Update scanner.c

--- scanner.c
+++ scanner.c
@@ -1,28 +1,6 @@
  #include"scanner.h"
 
-      
  
-char * substring(char *word, int left, int right)
-
-    {
-    int sub_length =right -left+1;
-     char *word_pointer =(char*) malloc(sizeof(char)*(sub_length + 1));  //the is to for the null string \0
-       
-    if (word_pointer == NULL) {
-        printf("Memory allocation failed.\n");
-        return NULL;
-    }  
-
-  
-    for (int i = left; i <= right && word[i] != '\0'; i++) {
-        *word_pointer = word[i];
-        word_pointer++;
-    }
-
-    *word_pointer = '\0';  // Append the null character
-
-    return word_pointer-sub_length;  // Return the original pointer
-}
 
     int is_operator(char ch)
     {
@@ -107,55 +85,8 @@ int is_real_num(char *word)
     
 }
 
-int tokens_genrator(char *source_code)
-{
-    int left = 0 , right = 0; //refrence identifiy the start and end of each token
-    int len =strlen(source_code);
 
-    while(right<=len && left<=right)
-    {
-        if(special_char(source_code[right])==0) // if the char not a special character skip it
-            right++;
-        
-        if(special_char(source_code[right])==1 && right==left)
-        {
-            if(is_operator(source_code[right]))
-                printf(" <%c,operator> \n",source_code[right]);
-            
-            else if(source_code[right]!=' ') //skip the witespace 
-                printf(" <%c,special character> \n",source_code[right]);
-            else if (source_code[right]==';')
-                printf(" <%c,semicolon>\n",source_code[right]);
-
-            //set the new refrence to the new token
-            right++;
-            left=right; 
-        }
-        else if(special_char(source_code[right])==1 && left!=right || (left!=right && right==len) )
-        {
-            //remove the delimeter or spa ce from the string
-            char *word =substring(source_code,left,right-1); 
-            if(iskeyword(word)==1)
-               printf(" <%s,keyword> \n",word);
-            else if (is_identifier(word)==1)
-                printf(" <%s,identifier> \n",word);
-            else if (is_integer(word)==1)
-                printf(" <%s,integer> \n",word);
-            else if (is_real_num(word)==1)
-                printf(" <%s,real number> ",word);
-            else
-                printf(" <%s,not in the language>",word);
-            
-            left =right;
-
-        } 
-
-    }
-    return 0;
-
-}
-
-void read_code(char **output_code,char * input_file)
+int read_file(char **output_code,char * input_file)
 {
       FILE *file=NULL;
 
@@ -164,23 +95,70 @@ void read_code(char **output_code,char * input_file)
     if (file != NULL)
     {
        char chr;
-        char *store_buffer=malloc(sizeof(char)*(max_lenght_of_code)); // this is an array of charcter and not a string 
-        int lenght =0;
+        char *buffer=malloc(sizeof(char)*(max_length_of_code)); // this is an array of charcter and not a string 
+        int length =0;
         while((chr = fgetc(file)) != EOF ) // loop until you see the end of the file
         {
             if(chr =='\n')    //skip the new line character 
             continue;
-            store_buffer[lenght]=chr;
-            lenght++;  // the lenght count additional one  aftter all caracter are stroed
+           buffer[length]=chr;
+            length++;  // the length count additional one  aftter all caracter are stroed
         
         }
         fclose(file);
-        store_buffer[lenght]= '\0'; // put termination charcter at the end o character stream
-        * output_code=store_buffer;
+        buffer[length]= '\0'; // put termination charcter at the end o character stream
+        * output_code=(char*) realloc(buffer,length*sizeof(char));
+        buffer=NULL;
+        
+        return length;
 
     }
     else
         printf("file does not exist");
-
+    return 0;
         
+}
+
+int tokens (char*words,int length)
+{
+    int current =0;
+    int word_counter=0;
+    int word_flage =0;
+    char * word = (char * ) malloc (word_max*sizeof(char));
+    
+    while ( current!=(length+1))
+    {
+        if( special_char(words[current])==0)
+        {
+            word[word_counter]=words[current];
+            word_counter++;
+            word_flage =1;
+            word[word_counter]='\0';
+        }
+        else if( special_char(words[current])==1)
+        {
+            if(word_flage==1)
+            {
+                word_counter=0;
+                word_flage=0;
+                if(iskeyword(word)==1)
+                    printf(" <%s,keyword> \n",word);
+                else if (is_identifier(word)==1)
+                     printf(" <%s,identifier> \n",word);
+                else if (is_integer(word)==1)
+                    printf(" <%s,integer> \n",word);
+                else if (is_real_num(word)==1)
+                    printf(" <%s,real number> ",word);
+                else
+                     printf(" <%s,not in the language>",word);
+            }
+
+            if(is_operator(words[current]))
+                printf(" <%c,operator> \n",words[current]); 
+            else if(words[current] !=' ') //skip the witespace 
+                printf(" <%c,special character> \n",words[current]);
+        }
+        current++;
+    }
+
 }
